6cf31ee76bcbabd089be3e7691e6f65b9cf8ee68
turkayurkmez <turkay.urkmez@gmail.com>
2021-09-13 16:38:34 +0300

    Add project files.

---
+++ miniShop/Infrastructure/ResponseDirectMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miniShop.Infrastructure
+{
+    public class ResponseDirectMiddleware
+    {
+        private RequestDelegate next;
+
+        public ResponseDirectMiddleware(RequestDelegate next)
+        {
+            this.next = next; 
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.Value.ToLower() =="/error")
+            {
+                await httpContext.Response.WriteAsync("Hata....");
+            }
+            await next.Invoke(httpContext);
+        }
+    }
+}
---
+++ miniShop/Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using miniShop.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miniShop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+           
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //Tüm pipeline'ı tek bir middleware'e yönlendir:
+            //app.Run(async (context) =>
+            //{
+            //    context.Response.ContentType = "text/plain";
+            //    await  context.Response.WriteAsync("Talep middleware'a ulasti");
+            //});
+
+            app.Map("/test", async (appBuilder) => {
+                appBuilder.Run(async (context) =>
+                {
+                    if (context.Request.Query.ContainsKey("flag"))
+                    {
+                        context.Response.ContentType = "text/utf8";
+                        await context.Response.WriteAsync($"Flag değeri: {context.Request.Query["flag"]} olarak geldi");
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync($"Flag değeri gelmediği  için test yapılamadı");
+
+                    }
+                });
+            });
+
+            app.UseMiddleware<ResponseDirectMiddleware>();
+
+           // app.UseWelcomePage();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
